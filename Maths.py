49768f3cfbfa561713e0eee3224d60a80ac0657c
AKASHDKR <87371893+AKASHDKR@users.noreply.github.com>
2021-07-17 14:12:01 +0530

    Add files via upload

---
+++ Maths.py
@@ -0,0 +1,12 @@
+print("Enter 1st no:")
+n1=int(input())
+print("Enter 2nd no:")
+n2=int(input())
+result1=n1+n2
+print("Result of addition:",result1)
+result2=n1-n2
+print("Result of subtraction:",result2)
+result3=n1*n2
+print("Result of multiplication:",result3)
+result4=n1/n2
+print("Result of division:",result4)
