dc0b69b8e87bb313cc016a31b20f933f0c55ed1f
AKASHDKR <akashdkr170901@gmail.com>
2021-07-21 20:41:53 +0530

    Completed

---
+++ basic/si.py
@@ -0,0 +1,8 @@
+print("Enter the principal amount:")
+p=int(input())
+print("Enter time of period:")
+t=int(input())
+print("Enter rate of interest:")
+r=int(input())
+si=p*t*r/100
+print("Simple interest:",si)
